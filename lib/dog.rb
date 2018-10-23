4a11b800678bbf03675080fe780674beb18a26d2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 15:12:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each { |dog| puts dog.name }
+  end
+end
